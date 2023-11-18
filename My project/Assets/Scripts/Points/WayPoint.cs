7d2bd2ff3200fdc57b1c2d5cfbb091dc9d52dc38
Vanzatoru <cimucarazvan23@yahoo.com>
2023-11-18 12:01:12 +0000

    Added project

---
+++ My project/Assets/Scripts/Points/WayPoint.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    public WayPoint previous;
+    public WayPoint next;
+    public WayPoint branch1;
+    public WayPoint branch2;
+    [SerializeField]
+    [Range(0f, 5f)]
+    public float width = 1f;
+
+
+
+    public Vector3 GetPosition()
+    {
+        Vector3 minBound = transform.position + transform.right * width / 2f;
+        Vector3 maxBound = transform.position - transform.right * width / 2f;
+        return Vector3.Lerp(minBound, maxBound, Random.Range(0f, 1f));
+    }
+}
